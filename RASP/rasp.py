81b9760b5e42ecb1dc9df27f1cc059bab1f8e906
klaus.begnis <kbegnis23@gmail.com>
2023-04-24 16:49:30 -0300

    checking if mqtt connection works

--- RASP/rasp.py
+++ RASP/rasp.py
@@ -1,6 +1,8 @@
 #Define Libraries
 import RPi.GPIO as gpio
 import time
+from paho.mqtt import client as mqtt_client
+import json
 
 #Configuring don’t show warnings 
 gpio.setwarnings(False)
@@ -24,12 +26,65 @@ gpio.setup(x_pin,gpio.OUT)
 gpio.setup(y_pin,gpio.OUT)
 
  
+def update_dc(xi,yi):
+    global dcX
+    global dcY
+
+    dcX = (xi/x_max)*100
+    dcY  = (yi/y_max)*100
+
+
 #Create the dutycycle variables
-dcX = (xi/x_max)*100
-dcY  = (yi/y_max)*100
+update_dc(xi,yi)
+
 print(dcX)
 print(dcY)
 
+def readMQTTconfig(path):
+    # opens json file and returns dictionaty
+    with open(path,'r') as file:
+        info = json.load(file)
+    return info
+
+def connectMqtt(client_json):
+    def on_connect(client, userdata, flags, rc):
+        if client._client_id != None: ###to debug only the write
+            if rc == 0:
+                print("MQTT connection sucesful")
+            else:
+                print(f"MQTT connection failed: rc{rc}")
+        else:
+            pass
+    def on_disconnect(client, userdata, rc):
+        print(f"Client disconnected code= {rc}")
+    
+    client = mqtt_client.Client(client_json["client"])
+    client.username_pw_set(client_json["user"], client_json["pwd"])
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.connect(client_json["broker"], client_json["port"])
+    return client
+            
+def readCoordinates(client,client_json):
+    def on_message(client, userdata, msg):
+        global xi
+        global yi
+        info = json.loads(msg.payload.decode())
+        xi = info["x_coord"]
+        yi = info["y_coord"]
+        update_dc(xi,yi)
+        print(dcX,dcY)
+
+    client.subscribe(client_json["topic"])
+    client.on_message = on_message
+    client.loop_forever()
+
+## creates mqtt connection
+
+config_path = r"./json/brokerinfo.json"
+mqtt_Config = readMQTTconfig(config_path)
+client = connectMqtt(mqtt_Config)
+readCoordinates(client)
 
 #Configure the pwm objects and initialize its value
 pwmX = gpio.PWM(x_pin,frequency)
