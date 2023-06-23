ea101b5c69c7b22fe2552726eb29ddfdd0d366c0
klaus.begnis <kbegnis23@gmail.com>
2023-06-23 09:17:38 -0300

    modifying to wait after conversion

--- RASP/rasp.py
+++ RASP/rasp.py
@@ -4,126 +4,103 @@ import time
 from paho.mqtt import client as mqtt_client
 import json
 
-#Configuring don’t show warnings 
-gpio.setwarnings(False)
+class GPIOmanager():
+    def __init__(self,x_max=0,y_max=0):
+        
+        #Configuring don’t show warnings 
+        gpio.setwarnings(False)
 
-# definitions
-x_pin = 27
-y_pin = 22
+        # definitions
+        self.x_pin = 27
+        self.y_pin = 22
 
-xi = 50 # value readed
-yi = 25 # value readed
+        self.xi = 50 # value readed
+        self.yi = 25 # value readed
 
-x_max = 100 #mm
-y_max = 100 #mm
-x_min = 0
-y_min = 0
+        self.x_max = x_max #mm
+        self.y_max = y_max #mm
+        self.x_min = 0
+        self.y_min = 0
 
-period = 0.1 #s
-frequency = 1/period # Hz
+        self.period = 0.1 #s
+        self.frequency = 1/self.period # Hz
 
-#Configuring GPIO
-gpio.setmode(gpio.BCM)
-gpio.setup(x_pin,gpio.OUT)
-gpio.setup(y_pin,gpio.OUT)
+        #Configuring GPIO
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.x_pin,gpio.OUT)
+        gpio.setup(self.y_pin,gpio.OUT)
 
-pwmX = gpio.PWM(x_pin,frequency)
-pwmX.start(0)
+        self.pwmX = gpio.PWM(self.x_pin,self.frequency)
+        self.pwmX.start(0)
 
-pwmY = gpio.PWM(y_pin,frequency)
-pwmY.start(0)
+        self.pwmY = gpio.PWM(self.y_pin,self.frequency)
+        self.pwmY.start(0)
+
+        ## creates mqtt connection
+
+        #config_path = r"./json/brokerInfo.json"
+        #mqtt_Config = self.readMQTTconfig(config_path)
+        #client = self.connectMqtt(mqtt_Config)
 
  
-def update_dc(xi,yi,pwnx,pwmy):
-
-    x_max = 100
-    y_max = 100
-    x_min = 0
-    y_min = 0
-    if xi >= x_max:
-        dcX = 99.99 #maximum value allowed from the library
-    elif xi <= x_min:
-        dcX = 0
-    else:
-        dcX = (xi/x_max)*100
-
-    if yi >= y_max:
-        dcY = 99.99 #maximum value allowed from the library
-    elif yi <= y_min:
-        dcY = 0
-    else:
-        dcY  = (yi/y_max)*100
-
-    pwnx.ChangeDutyCycle(dcX)
-    pwmy.ChangeDutyCycle(dcY)
+    def update_dc(self,xi,yi):
 
 
-#Create the dutycycle variables
-
-
-def readMQTTconfig(path):
-    # opens json file and returns dictionaty
-    with open(path,'r') as file:
-        info = json.load(file)
-    return info
-
-def connectMqtt(client_json):
-    def on_connect(client, userdata, flags, rc):
-        if client._client_id != None: ###to debug only the write
-            if rc == 0:
-                print("MQTT connection successful")
-            else:
-                print(f"MQTT connection failed: rc{rc}")
+        if xi >= self.x_max:
+            dcX = 99.99 #maximum value allowed from the library
+        elif xi <= self.x_min:
+            dcX = 0
         else:
-            pass
-    def on_disconnect(client, userdata, rc):
-        print(f"Client disconnected code= {rc}")
-    
-    client = mqtt_client.Client(client_json["client"])
-    client.username_pw_set(client_json["user"], client_json["pwd"])
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.connect(client_json["broker"], client_json["port"])
-    return client
-            
-def readCoordinates(client_json,client,pwmx,pwmy):
-    def on_message(client, userdata, msg):
-        global xi
-        global yi
-        info = json.loads(msg.payload.decode())
-        xi = info["x_coord"]
-        yi = info["y_coord"]
-        update_dc(xi,yi,pwmx,pwmy)
-        print(xi,yi)
+            dcX = (xi/self.x_max)*100
 
-    client.subscribe(client_json["topic"])
-    client.on_message = on_message
-    client.loop_forever()
+        if yi >= self.y_max:
+            dcY = 99.99 #maximum value allowed from the library
+        elif yi <= self.y_min:
+            dcY = 0
+        else:
+            dcY  = (yi/self.y_max)*100
 
-## creates mqtt connection
+        self.pwnx.ChangeDutyCycle(dcX)
+        self.pwmy.ChangeDutyCycle(dcY)
 
-config_path = r"./json/brokerInfo.json"
-mqtt_Config = readMQTTconfig(config_path)
-client = connectMqtt(mqtt_Config)
-readCoordinates(mqtt_Config,client,pwmX,pwmY)
+    def setMAXvalues(self,x,y):
+        self.x_max = x
+        self.y_max = y
+
+    #Create the dutycycle variables
 
 
+    #def readMQTTconfig(self,path):
+    #    # opens json file and returns dictionaty
+    #    with open(path,'r') as file:
+    #        info = json.load(file)
+    #    return info
 
-#Configure the pwm objects and initialize its value
-
-update_dc(xi,yi)
-
-
-
-#Loop infinite
-while True:
-    try:
-        #increment gradually the luminosity
-        time.sleep(0.5)
-
-            
-    except Exception as e:
-        print(e)
-        #End code
-        gpio.cleanup()
-        exit()
+    #def connectMqtt(self,client_json):
+    #    def on_connect(client, userdata, flags, rc):
+    #        if client._client_id != None: ###to debug only the write
+    #            if rc == 0:
+    #                print("MQTT connection successful")
+    #            else:
+    #                print(f"MQTT connection failed: rc{rc}")
+    #        else:
+    #            pass
+    #    def on_disconnect(client, userdata, rc):
+    #        print(f"Client disconnected code= {rc}")
+    #    
+    #    client = mqtt_client.Client(client_json["client"])
+    #    client.username_pw_set(client_json["user"], client_json["pwd"])
+    #    client.on_connect = on_connect
+    #    client.on_disconnect = on_disconnect
+    #    client.connect(client_json["broker"], client_json["port"])
+    #    return client
+                
+    #def readCoordinates(self,client_json,client,pwmx,pwmy):
+    #        def on_message(client, userdata, msg):
+    #            global xi
+    #            global yi
+    #            info = json.loads(msg.payload.decode())
+    #            xi = info["x_coord"]
+    #            yi = info["y_coord"]
+    #            self.update_dc(xi,yi,pwmx,pwmy)
+    #            print(xi,yi)
