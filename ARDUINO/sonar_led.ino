93ea13d2f9bf42ef8cb149b7953e8b1065b9abc7
klaus.begnis <kbegnis23@gmail.com>
2023-06-28 20:23:22 -0300

    ARDUINO step motor fixed plus servo control

--- ARDUINO/sonar_led.ino
+++ ARDUINO/sonar_led.ino
@@ -1,3 +1,7 @@
+#include <Servo.h>
+
+// I/O DEFINITIONS
+
 //#define LED 52
 #define ECHO 51
 #define TRIG 50
@@ -8,13 +12,42 @@
 #define bro 47
 #define yel 48
 #define red 49
-//10,10,10,10
-//lm,av,ml,va
-//RYBO
+
+// SERVO PINS
+#define servo1 9 // Pino de controle do servo 1
+#define servo2 10 // Pino de controle do servo 2
+#define servo3 11 // Pino de controle do servo 3
+
+Servo s1;
+Servo s2;
+Servo s3;
+
+// SERVO MOTOR'S VARIABLES
+
+float L1 = 120; //altura até o primeiro palito na barra roscada em cm (Partindo da base do motor de passo)
+float L2 = 120; //Comprimento do primeiro palito (distância da barra roscada até o primeiro servo)
+float L3 = 120; //Comprimento do segundo palito (distância do primeiro servo - grande - até o segundo servo - pequeno)
+float Le = 120; //Comprimento do último palito (distância do segundo servo - pequeno - até o sensor de distância)
+
+float r;
+float h;
+float alpha;
+float beta;
+float gama;
+float z = 5; // mm objective height
+
+float theta1 = 0; //Referente ao StepMotor - motor de passo (valor inicial configurável)
+float theta2 = 0; //Referente ao Servo2 - motor servo grande (valor inicial configurável)
+float theta3 = 0; //Referente ao Servo3 - motor servo pequeno (valor inicial configurável)
+
+// STEP MOTOR VARIABLES
+
 int OB = 0b0001;
 int BO = 0b0010;
 int YR = 0b0100;
 int RY = 0b1000;
+
+// CONVERSION VARIABLES
 
 float X_MAX = 190;
 float Y_MAX = 160;
@@ -30,6 +63,8 @@ double y_reading;
 double distance;
 double realCoordinates;
 
+// struct's definitions
+
 struct coordinate
 {
   double x;
@@ -40,54 +75,108 @@ struct stepMotor
 {
     int startPin = ora;
     int currentStep = OB;
+    int current_position=0;
+    float degreePerStep = 1;
 
-    void sendStepConfig()
+    void moveToPosition(float theta)
+    {
+      if (theta > 0)
+      {
+        while (current_position < theta)
+        {
+          Serial.println(current_position);
+          walkOneStepRight();
+          current_position += degreePerStep;
+          if (not(current_position + degreePerStep >= theta))
+          {
+            if (currentStep == RY)
+            {
+                currentStep = OB;
+            }
+            else {
+                currentStep = currentStep*0b0010;
+            }
+          }
+        }
+      }
+      else 
+      {
+        while (current_position > theta)
+        {
+          Serial.println(current_position);
+          walkOneStepLeft();
+          current_position -= degreePerStep;
+          if (not(current_position - degreePerStep <= theta))
+          {
+            if (currentStep == OB)
+            {
+                currentStep = RY;
+            }
+            else {
+                currentStep = currentStep/0b0010;
+            }
+          }
+        }
+      }
+    }
+
+    void walkOneStepRight()
     {
         for (int i = 0; i < 4; i+=1)
         {
-            digitalWrite(startPin+i,(1 <= (currentStep & (1 << i))));
+            //digitalWrite(startPin+i,(1 <= (currentStep & (1 << i))));
+            Serial.print((1 <= (currentStep & (1 << i))));
         }
         delay(50);
+        Serial.println();
         for (int i = 0; i < 4; i+=1)
         {
-            digitalWrite(startPin+i,0);
+            //digitalWrite(startPin+i,0);
+            continue;
         }
-        if (currentStep == RY)
+    }
+    void walkOneStepLeft()
+    {
+        for (int i = 0; i < 4; i++)
         {
-            currentStep = OB;
+            //digitalWrite(startPin+i,(1 <= (currentStep & (1 << i))));
+            Serial.print((1 <= (currentStep & (1 << i))));
         }
-        else {
-            currentStep = currentStep*0b0010;
-
+        delay(50);
+        Serial.println();
+        for (int i = 0; i < 4; i+=1)
+        {
+            //digitalWrite(startPin+i,0);
+            continue;
         }
-
     }
 
 };
 
+// struct initialization
 stepMotor STEP_MOTOR;
 coordinate REAL_COORDINATE; 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(57600);
   pinMode(ECHO,INPUT);
   pinMode(TRIG,OUTPUT);
-  //pinMode(LED,OUTPUT);
   pinMode(X_READ,INPUT);
   pinMode(Y_READ,INPUT);
   pinMode(red,OUTPUT);
   pinMode(yel,OUTPUT);
   pinMode(ora,OUTPUT);
   pinMode(bro,OUTPUT);
-
-  STEP_MOTOR.sendStepConfig();
+  s2.attach(servo2); //ASSOCIAÇÃO DO PINO DIGITAL AO OBJETO DO TIPO SERVO
+  s2.write(theta2); //Também precisamos definir onde vai ser o 0 desses servos
+  s3.attach(servo3); //ASSOCIAÇÃO DO PINO DIGITAL AO OBJETO DO TIPO SERVO
+  s3.write(theta3); //Também precisamos definir onde vai ser o 0 desses servos
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  distance = readDistance(); //mm
-  Serial.println(distance);
+  //distance = readDistance(); //mm
+  //Serial.println(distance);
   /*
   if (distance < 150){
     //setLedAndWait(0, HIGH,false);
@@ -101,16 +190,11 @@ void loop() {
   
   x_pulse = pulseIn(X_READ,HIGH);
   y_pulse = pulseIn(Y_READ,HIGH);
-
-  // max time to pulse DC=100 -> T=0.01 -> 10ms
-  // (10ms-0) / 100 = 0.1
-  x_reading = x_pulse*X_MAX/PERIOD_MAX/1000; // trocar para *?
+  x_reading = x_pulse*X_MAX/PERIOD_MAX/1000;
   y_reading = y_pulse*Y_MAX/PERIOD_MAX/1000;
 
   Serial.print("Coordenada X: ");
-  Serial.println(x_reading);
-
-  
+  Serial.println(x_reading); 
   Serial.print("Coordenada Y: ");
   Serial.println(y_reading);
 
@@ -118,17 +202,10 @@ void loop() {
 
   Serial.print("Converted X: ");
   Serial.print(REAL_COORDINATE.x);
-
   Serial.print("Converterd Y: ");
   Serial.print(REAL_COORDINATE.y);
-
-
-  STEP_MOTOR.sendStepConfig();
-}
-
-void setLedAndWait(int ts, bool setter,bool wait){
-  //digitalWrite(LED,setter);
-  if (wait){delay(ts);};
+  
+  //go2objective(REAL_COORDINATE);
 }
 
 double readDistance()
@@ -163,4 +240,25 @@ coordinate convert2Reference(double x, double y)
   RESULT.x = x_converted;
   RESULT.y = y_converted;
   return RESULT;
+}
+
+void go2objective(coordinate COORD)
+{
+  float x = COORD.x;
+  float y = COORD.y;
+
+  theta1 = atan2(x, y)*180/PI; // step motor angle
+
+  r = sqrt(pow(x,2) + pow((y-L2),2));
+  h = sqrt(pow((L1-z),2) + pow(r,2));
+  alpha = acos((pow(L3,2) + pow(Le,2) - pow(h,2))/(2*L3*Le))*180/PI;
+  gama = acos((pow(L3,2) + pow(h,2) - pow(L3,2))/(2*L3*h))*180/PI;
+  beta = atan2(r, L1-z)*180/PI;
+
+  theta2 = 90 - gama - beta; // elbow angle
+  theta3 = 180 - alpha; // wrist angle
+
+  s2.write(theta1);
+  s3.write(theta3);
+  STEP_MOTOR.moveToPosition(theta3);
 }
